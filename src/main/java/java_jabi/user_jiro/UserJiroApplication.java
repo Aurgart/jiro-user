e0514b8b779cd82fe052b84acde71b305e22052d
Aurgart <ilyalemeshew@gmail.com>
2026-03-03 12:04:07 +0300

    Init master

---
+++ src/main/java/java_jabi/user_jiro/UserJiroApplication.java
@@ -0,0 +1,13 @@
+package java_jabi.user_jiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserJiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserJiroApplication.class, args);
+	}
+
+}
